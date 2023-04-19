e5ef524aa35906eb33a8aca2ef3f4d301011c64e
nejedly <nejedly@isibrno.cz>
2023-04-19 15:08:54 +0200

    mef3 read data with transforms

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np
 import torch
 from neurobox.dataloaders.dfile import DFile
-from neurobox.dataloaders.mef3 import Mef3
+from neurobox.dataloaders.mef3 import Mef3,Mef3RAM
 from neurobox.dataloaders.h5 import H5
 from neurobox.pipelines import pipeline_torch,pipeline_numpy
 import unittest
@@ -20,6 +20,14 @@ class test(unittest.TestCase):
     def test_mef3(self):
         file = "/mnt/m/d04/eeg_data/kuna_eeg/fnusa_dataset/sub-102/ses-001/ieeg/sub-102_ses-001_task-rest_run-01_ieeg.mefd"
         M = Mef3(session_path=file, password='bemena')
+        bi = M.read_ts_channel_basic_info_df()
+        for data, meta in SessionSampleIterator(session=M,window=15000,step=5000).select_channels(channel_map=['B1','C1']):
+            stop = 1
+        assert True
+
+    def test_mef3ram(self):
+        file = "/mnt/m/d04/eeg_data/kuna_eeg/fnusa_dataset/sub-102/ses-001/ieeg/sub-102_ses-001_task-rest_run-01_ieeg.mefd"
+        M = Mef3RAM(session_path=file, password='bemena')
         bi = M.read_ts_channel_basic_info_df()
         for data, meta in SessionSampleIterator(session=M,window=15000,step=5000).select_channels(channel_map=['B1','C1']):
             stop = 1
@@ -75,10 +83,13 @@ class test(unittest.TestCase):
         stop = 1
 
     def test_bipolar_pipeline(self):
-        file = "/mnt/m/d05/hfo_envelopes/sleep/seeg-102/raw_files/seeg-102_00.h5"
-        H = H5(path=file)
+        # file = "/mnt/m/d05/hfo_envelopes/sleep/seeg-102/raw_files/seeg-102_00.h5"
+        # H = H5(path=file)
+        file = "/mnt/m/d04/eeg_data/kuna_eeg/fnusa_dataset/sub-102/ses-001/ieeg/sub-102_ses-001_task-rest_run-01_ieeg.mefd"
+        M = Mef3RAM(session_path=file, password='bemena')
 
-        I = SessionBipolarSampleIterator(H,window=5000,step=5000)
+
+        I = SessionBipolarSampleIterator(M,window=5000,step=5000)
 
         class RelEntropy:
             classNames = ['RelEntropy']
--- neurobox/dataloaders/mef3/__init__.py
+++ neurobox/dataloaders/mef3/__init__.py
@@ -1,1 +1,2 @@
 from .mef3 import Mef3
+from .mef3ram import Mef3RAM
