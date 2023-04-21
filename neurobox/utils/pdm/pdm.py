ae2700ad4535c1c6e540e1394140ad9747b6f81f
nejedly <nejedly@isibrno.cz>
2023-04-21 18:26:32 +0200

    PDM channel sort

--- neurobox/utils/pdm/pdm.py
+++ neurobox/utils/pdm/pdm.py
@@ -4,10 +4,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import zscore
 
-def generate_PDM_matrix(df,feature_name,time_axis='start_sample',index_axis='channel_name',plot=False,title=None,save_plot=None,normalize=False) -> pd.DataFrame:
+def generate_PDM_matrix(df,feature_name,time_axis='start_sample',index_axis='channel_name',plot=False,title=None,save_plot=None,normalize=False,sort_channels=False) -> pd.DataFrame:
     PDM = pd.pivot_table(df, values=feature_name, columns=time_axis, index=index_axis)
     PDM = PDM.reset_index()
-    PDM = channel_sort_df(PDM, 'channel_name')
+
+    if isinstance(sort_channels,bool) and sort_channels:
+        PDM = channel_sort_df(PDM, 'channel_name')
+
+    if isinstance(sort_channels,list):
+        PDM['electrode_id'] = PDM['channel_name'].apply(lambda x: sort_channels.index(x))
+        PDM = PDM.sort_values(by='electrode_id')
+        PDM = PDM.drop(columns=['electrode_id'])
+
     PDM = PDM.set_index('channel_name', drop=True)
     if plot:
         plt.figure(dpi=300)
