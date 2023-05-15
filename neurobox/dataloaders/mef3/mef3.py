69a39a7b7f6b8a8e791f39887ec89c9e3fa69f19
Petr <xnejed07@gmail.com>
2023-05-15 16:18:57 +0100

    mef3 deploy models

--- neurobox/dataloaders/mef3/mef3.py
+++ neurobox/dataloaders/mef3/mef3.py
@@ -2,6 +2,7 @@ from pymef import MefSession
 import pandas as pd
 from neurobox.utils import channel_sort_df
 import numpy as np
+from tqdm import tqdm
 
 class Mef3(MefSession):
     def __init__(self,session_path,password,transforms=None):
@@ -22,7 +23,7 @@ class Mef3(MefSession):
 
     def read_ts_channels_sample(self, channel_map, sample_map, process_n=None):
         data = super().read_ts_channels_sample(channel_map,sample_map,process_n)
-        if transforms:
+        if self.transforms:
             data = [self.transforms(x) for x in data]
         return data
 
@@ -31,5 +32,28 @@ class Mef3(MefSession):
             sample_map = [0,self._bi.iloc[0]['nsamp']]
 
         for ch in self._bi['name'].tolist():
-            data = self.read_ts_channels_sample(channel_map=[ch],sample_map=sample_map)
+            data = self.read_ts_channels_sample(channel_map=[ch],sample_map=sample_map)[0]
             yield ch, data
+
+    def itersegments(self,sample_window):
+        for ch in self._bi['name'].tolist():
+            start_sample = 0
+            while (start_sample + sample_window) < self._bi.iloc[0]['nsamp']:
+                stop_sample = start_sample+sample_window
+                data = self.read_ts_channels_sample(channel_map=[ch],sample_map=[start_sample,stop_sample])[0]
+                yield ch, start_sample, stop_sample, data
+                start_sample += sample_window
+
+
+    def deploy_model(self,model,sample_window):
+        output = []
+        for ch, start_sample, stop_sample, data in tqdm(self.itersegments(sample_window)):
+            y = model.run(data)
+            output.append({'channel_name':ch,
+                           'start_sample': start_sample,
+                           'stop_sample':stop_sample,
+                           'data':y})
+
+        output = pd.DataFrame(output)
+        return output
+
