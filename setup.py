e4e9b61611631853a1236db37a426ce98ed20879
nejedly <nejedly@isibrno.cz>
2023-10-09 10:11:18 +0200

    ArrayFile

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='',
     packages=['neurobox', 'neurobox.utils', 'neurobox.utils.pdm', 'neurobox.iterators', 'neurobox.pipelines',
               'neurobox.dataloaders', 'neurobox.dataloaders.h5', 'neurobox.dataloaders.mef3',
-              'neurobox.dataloaders.dfile'],
+              'neurobox.dataloaders.dfile', 'neurobox.dataloaders.array'],
     url='',
     license='',
     author='nejedly',
